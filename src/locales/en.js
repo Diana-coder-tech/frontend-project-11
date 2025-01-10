1a422bc0f3c57472e5496c9f336623f1923f2dd9
Diana Zhiliakova <zhiliackova2009@yandex.ru>
2025-01-10 16:44:27 +0300

    change after lint

--- src/locales/en.js
+++ src/locales/en.js
@@ -1,17 +1,16 @@
 export default {
-    translation: {
-      success: 'Successfuly uploud',
-      errors: {
-        invalidUrl: 'URL invalid',
-        alreadyAddedRSS: 'You have this URL',
-        parseError: 'RSS not valid',
-        mustNotBeEmpty: 'RSS must be not null',
-        NetworkError: 'Network Error',
-        defaultError: 'Something went wrong',
-      },
-      preview: 'Preview',
-      feeds: 'Feeds',
-      posts: 'Posts',
+  translation: {
+    success: 'Successfuly uploud',
+    errors: {
+      invalidUrl: 'URL invalid',
+      alreadyAddedRSS: 'You have this URL',
+      parseError: 'RSS not valid',
+      mustNotBeEmpty: 'RSS must be not null',
+      NetworkError: 'Network Error',
+      defaultError: 'Something went wrong',
     },
-  };
-  
+    preview: 'Preview',
+    feeds: 'Feeds',
+    posts: 'Posts',
+  },
+};
--- src/parser.js
+++ src/parser.js
@@ -1,29 +1,28 @@
 const parse = (data) => {
-    const parser = new DOMParser();
-    const parsedData = parser.parseFromString(data, 'application/xml');
-    const errorNode = parsedData.querySelector('parsererror');
-    if (errorNode) {
-      const error = new Error(errorNode.textContent);
-      error.isParsingError = true;
-      throw error;
-    }
-    const channel = parsedData.querySelector('channel');
-    const title = channel.querySelector('title').textContent;
-    const description = channel.querySelector('description').textContent;
-    const feed = { title, description };
-    const items = Array.from(parsedData.querySelectorAll('item'));
-    const posts = items.map((item) => {
-      const postLink = item.querySelector('link').textContent;
-      const postTitle = item.querySelector('title').textContent;
-      const postDescription = item.querySelector('description').textContent;
-      return {
-        link: postLink,
-        title: postTitle,
-        description: postDescription,
-      };
-    });
-    return { feed, posts };
-  };
+  const parser = new DOMParser();
+  const parsedData = parser.parseFromString(data, 'application/xml');
+  const errorNode = parsedData.querySelector('parsererror');
+  if (errorNode) {
+    const error = new Error(errorNode.textContent);
+    error.isParsingError = true;
+    throw error;
+  }
+  const channel = parsedData.querySelector('channel');
+  const title = channel.querySelector('title').textContent;
+  const description = channel.querySelector('description').textContent;
+  const feed = { title, description };
+  const items = Array.from(parsedData.querySelectorAll('item'));
+  const posts = items.map((item) => {
+    const postLink = item.querySelector('link').textContent;
+    const postTitle = item.querySelector('title').textContent;
+    const postDescription = item.querySelector('description').textContent;
+    return {
+      link: postLink,
+      title: postTitle,
+      description: postDescription,
+    };
+  });
+  return { feed, posts };
+};
   
-  export default parse;
-  
+export default parse;
